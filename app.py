ec7e443113d327a299241e3aa22987442105ab23
haugstve <daniel.haugstvedt@gmail.com>
2020-03-09 16:45:33 +0100

    Refactor and update code

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
-import pandas as pd
+# import pandas as pd
 from flask import Flask, jsonify, request
 from data_prep import *
+import pickle
 
 # connect to db
 
+
+with open('models/rf_pipe.pkl','rb') as f:
+    MODEL = pickle.loads(f)
+
 # get data
-data = pd.read_csv('Dataset/titanic.csv')
+DATA = pd.read_csv('Dataset/titanic.csv')
 
 # app
 app = Flask(__name__)
 
+print(DATA.sample(1))
+print(MODEL.predict(DATA.sample(1)))
+
 # routes
 @app.route('/', methods=['POST'])
+def predict():
 
-def data_prep():
-
+    print(DATA.sample(1))
+    print(MODEL.predict(DATA.sample(1)))
     #data = clean_data(data)
 
     #data = data.head()
 
     #return jsonify(data)
-    return jsonify(test_function())#jsonify(results='This will return prepared data. Soon. Hopefully.')
+    return jsonify(MODEL.predict())#jsonify(results='This will return prepared data. Soon. Hopefully.')
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
