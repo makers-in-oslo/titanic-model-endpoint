7368d1aff83033e8a38e6df674d1c9bf4c52fd33
haugstve <daniel.haugstvedt@gmail.com>
2020-03-09 17:03:17 +0100

    Include model

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ import pickle
 
 
 with open('models/rf_pipe.pkl','rb') as f:
-    MODEL = pickle.loads(f)
+    MODEL = pickle.load(f)
 
 # get data
-DATA = pd.read_csv('Dataset/titanic.csv')
+DATA = (pd.read_csv('Dataset/titanic.csv')
+          .rename(columns=str.lower)
+          .drop(columns=['survived','name','ticket','cabin'])
+          .set_index('passengerid'))
 
 # app
 app = Flask(__name__)
@@ -23,13 +26,13 @@ print(MODEL.predict(DATA.sample(1)))
 def predict():
 
     print(DATA.sample(1))
-    print(MODEL.predict(DATA.sample(1)))
+    prediction = MODEL.predict(DATA.sample(1))
     #data = clean_data(data)
 
     #data = data.head()
 
     #return jsonify(data)
-    return jsonify(MODEL.predict())#jsonify(results='This will return prepared data. Soon. Hopefully.')
+    return str(prediction[0]) #jsonify(results='This will return prepared data. Soon. Hopefully.')
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
